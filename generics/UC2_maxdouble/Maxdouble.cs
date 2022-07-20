088db3ee5917ad188c4c52f6d8a3456c17f7fc55
hibaanj <anjumhiba15@gmail.com>
2022-07-20 17:24:50 +0530

    max number among 3 double values

---
+++ generics/UC2_maxdouble/Maxdouble.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_maxdouble
+{
+    internal class Maxdouble
+    {
+        
+            public static void MaxDouble(double firstv, double secondv, double thiredv)
+            {
+                if ((firstv.CompareTo(secondv))>0 && (firstv.CompareTo(thiredv)>0))
+                {
+                    Console.WriteLine($"{firstv}is the greatest");
+                }
+                else if ((secondv.CompareTo(firstv))>0 && (secondv.CompareTo(thiredv)>0))
+                {
+                    Console.WriteLine($"{secondv}is the greatest");
+                }
+                else if ((thiredv.CompareTo(firstv)>0) && (thiredv.CompareTo(secondv)>0))
+                {
+                    Console.WriteLine($"{thiredv}is the greatest");
+                }
+
+            }
+
+        
+    }
+}
---
+++ generics/UC2_maxdouble/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_maxdouble
+{
+    class MaxDouble
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("testcase-1- returns first element since its the greatest");
+            Maxdouble.MaxDouble(14.70, 9.4,3.7);
+            Console.WriteLine(" ");
+            
+            Console.WriteLine("testcase-2- returns second element since its the greatest");
+
+            Maxdouble.MaxDouble(12.5, 34.2, 7.8);
+            Console.WriteLine(" ");
+
+           
+            Console.WriteLine("testcase-3- returns third element since its the greatest");
+            Maxdouble.MaxDouble(1.4, 3.5, 99.99);
+        }
+
+    }
+}
