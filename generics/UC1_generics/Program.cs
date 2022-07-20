3dd9284c26de319c4ed0b62b6b5c852717f7fcfe
hibaanj <anjumhiba15@gmail.com>
2022-07-20 17:03:01 +0530

    3 testcases to find maximum of integers

---
+++ generics/UC1_generics/Program.cs
@@ -0,0 +1,23 @@
+﻿//finding maximum integer
+using System;
+
+namespace UC1_generics
+{
+    class Maxint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("testcase-1");
+        
+            integer.MaxInt(12, 2, 3);
+
+            Console.WriteLine("testcase-2");
+
+            integer.MaxInt(1, 45, 7);
+
+            Console.WriteLine("testcase-3");
+            integer.MaxInt(2, 5, 9);
+        }
+
+    }
+}
---
+++ generics/UC1_generics/integer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_generics
+{
+    internal class integer
+    {
+        public static void MaxInt(int firstv, int secondv, int thiredv)
+        {
+            if((firstv > secondv)  && (firstv > thiredv))
+            {
+                Console.WriteLine($"{firstv}is the greatest");
+            } 
+            else if((secondv > firstv) && (secondv > thiredv))
+            {
+                Console.WriteLine($"{secondv}is the greatest");
+            }
+            else if ((thiredv > firstv) && (thiredv > secondv))
+            {
+                Console.WriteLine($"{thiredv}is the greatest" );
+            } 
+
+        }
+        
+    }
+}
