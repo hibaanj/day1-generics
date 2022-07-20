ac22d9cf0cb029ffdbe9ec91e66de04926eff21b
hibaanj <anjumhiba15@gmail.com>
2022-07-20 21:27:15 +0530

    max nujmber using generic at mehods level and class level

---
+++ generics/UC5_usinggenerics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC5_usinggenerics
+{
+    class GENerics
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("testcase-1- returns greatest element using integer and same method");
+            UC5_generics<int>.MAx(1111, 1111111, 111);
+
+            Console.WriteLine("testcase-2-return greatest using double elements but same methods");
+            UC5_generics<double>.MAx(2.5, 1.5, 77.2  );
+
+            Console.WriteLine("testcase-3- returns greatest using string and same method");
+            UC5_generics<string>.MAx("apple", "banana", "peach");
+
+        }
+
+    }
+}
---
+++ generics/UC5_usinggenerics/UC5_generics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC5_usinggenerics
+{
+    internal class UC5_generics<t> where t : IComparable 
+    {
+        public static t val;
+
+        public static t MAx (t st1, t st2, t st3)
+        {
+            if (st1.CompareTo(st2) > 0 && (st1.CompareTo(st2)) > 0 ||
+                (st1.CompareTo(st2)) >= 0 && (st1.CompareTo(st2)) > 0 ||
+                (st1.CompareTo(st2)) > 0 && (st1.CompareTo(st2)) >= 0)
+                {
+                    //return val = st1;
+                    Console.WriteLine(st1);
+                }
+
+
+            else if ((st2.CompareTo(st1)) > 0 && (st2.CompareTo(st3)) > 0 ||
+                (st2.CompareTo(st1)) >= 0 && (st2.CompareTo(st3)) > 0 ||
+                (st2.CompareTo(st1)) > 0 && (st2.CompareTo(st3)) >= 0)
+                {
+                    //return val = st2;
+                    Console.WriteLine(st2);
+
+                }
+                
+
+            else if ((st3.CompareTo(st1)) > 0 && (st3.CompareTo(st2)) > 0 ||
+                (st3.CompareTo(st1)) >= 0 && (st3.CompareTo(st2)) > 0 ||
+                (st3.CompareTo(st1)) > 0 && (st3.CompareTo(st2)) >= 0)
+                {
+                    //return val = st3;
+                    Console.WriteLine(st3);
+                }
+            return val;
+        }
+        
+    }
+}
