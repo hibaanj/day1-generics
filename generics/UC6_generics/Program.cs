46a0dc11fe9e2905ba33cd749909fccb36d2241c
hibaanj <anjumhiba15@gmail.com>
2022-07-21 19:08:32 +0530

    generics and array

--- generics/UC6_generics/Program.cs
+++ generics/UC6_generics/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6_generics
+{
+    class GENerics
+    {
+        public static void Main(string[] args)
+        {
+            int[] intarray = { 1, 2, 3, 4, 5, 5234 };
+            //UC6_generics<int> generics = new UC6_generics<int>[intarray];
+            UC6_generics<int>.MAx();
+
+        }
+
+    }
+}
--- generics/UC6_generics/UC6_generics.cs
+++ generics/UC6_generics/UC6_generics.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace UC6_generics
 {
-    internal class UC6_generics
+    internal class UC6_generics<t> where t : IComparable
     {
+        
+            public static t val;
+
+        public static t MAx(params int[] intarray )
+        {
+            int sum = 0;
+            foreach (int elements in intarray)
+            {
+                
+                sum += elements;
+
+                
+
+            }
+            Console.WriteLine(sum);
+
+
+            return val ;
+        }
+        
+
+        
     }
 }
